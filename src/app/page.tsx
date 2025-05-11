7b622b13d4b37dc525c4fef0c50267e56901b7eb
Your Name <you@example.com>
2025-05-11 17:38:17 +0000

    a

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { HeroSection } from "@/components/ui/hero";
-
-export default function Home() {
-  return <HeroSection />;
-}
--- src/components/max_width_wrapper.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MaxWidthWrapper({ children }: { children: ReactNode }) {
-  return <div className="mx-auto w-full md:pl-2.5">{children}</div>;
-}
